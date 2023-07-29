39e9619c5d5b8cf4ef2614a7aa45775b28f77f92
chaitracharu <chethumanju1998@gmail.com>
2023-07-29 13:05:53 +0530

    pushed by charu

---
+++ src/test/java/Script/Test4.java
@@ -0,0 +1,15 @@
+package Script;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import generic.BaseTest;
+
+public class Test4 extends BaseTest {
+	@Test
+	public void test4()
+	{
+		Reporter.log("Test4....",true);
+	}
+
+}
