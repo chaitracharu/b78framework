a20798bbd2f1f757dca8c1443356157dfa288f98
chaitracharu <chethumanju1998@gmail.com>
2023-07-30 09:22:25 +0530

    Automation script for testcase2

--- src/test/java/Script/Test2.java
+++ src/test/java/Script/Test2.java
@@ -1,15 +1,5 @@
 package Script;
 
-import org.testng.Reporter;
-import org.testng.annotations.Test;
-
-import generic.BaseTest;
-
-public class Test2 extends BaseTest {
-	@Test
-	public void test2()
-	{
-		Reporter.log("Test2....",true);
-	}
+public class Test2 {
 
 }
